4c19a72a11392b78347e2115df99da3acae437f3
khoatd2611 <khoa261199@hotmail.com>
2023-06-21 12:59:21 +0100

    order dao

--- src/dao/OrderDao.java
+++ src/dao/OrderDao.java
@@ -1,4 +1,3 @@
-
 package dao;
 
 import connection.MyConnection;
@@ -10,35 +9,109 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import java.sql.Time;
 
 /**
  *
  * @author tuan1
  */
 public class OrderDao {
-     Connection con = MyConnection.getConnetion();
+
+    Connection con = MyConnection.getConnetion();
     PreparedStatement ps;
     Statement st;
     ResultSet rs;
-    
-    public void insert(String id,String username,String email,String pass,String phone,String seq,String ans,String address ){
-String sql="insert into user values(?,?,?,?,?,?,?,?)  ";
+    // Tạo order mới
+
+    public void createOrder(int orderId, String userId, int packageId, Time pickupTime, String customerName, String customerAddress, String customerPhone) {
+        String sql = "INSERT INTO orders (order_id, user_id, package_id, pickup_time, customer_name, customer_address, customer_phone) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
-            ps= con.prepareStatement(sql);
-            ps.setString(1,id);
-            ps.setString(2, username);
-            ps.setString(3, email);
-            ps.setString(4, pass);
-            ps.setString(5, phone);
-            ps.setString(6, seq);
-            ps.setString(7, ans);
-            ps.setString(8, address);
-            
-            if(ps.executeUpdate()>0){
-                JOptionPane.showMessageDialog(null, "Order  added  successfully");
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, orderId);
+            ps.setString(2, userId);
+            ps.setInt(3, packageId);
+            ps.setTime(4, pickupTime);
+            ps.setString(5, customerName);
+            ps.setString(6, customerAddress);
+            ps.setString(7, customerPhone);
+
+            if (ps.executeUpdate() > 0) {
+                System.out.println("New order added successfully.");
             }
         } catch (SQLException ex) {
-            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         }
-}
+    }
+
+    // Đọc thông tin order
+    public void readOrder(int orderId) {
+        String sql = "SELECT * FROM orders WHERE order_id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, orderId);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("order_id");
+                String userId = rs.getString("user_id");
+                int packageId = rs.getInt("package_id");
+                Time pickupTime = rs.getTime("pickup_time");
+                String customerName = rs.getString("customer_name");
+                String customerAddress = rs.getString("customer_address");
+                String customerPhone = rs.getString("customer_phone");
+
+                System.out.println("Order ID: " + id);
+                System.out.println("User ID: " + userId);
+                System.out.println("Package ID: " + packageId);
+                System.out.println("Pickup Time: " + pickupTime);
+                System.out.println("Customer Name: " + customerName);
+                System.out.println("Customer Address: " + customerAddress);
+                System.out.println("Customer Phone: " + customerPhone);
+            } else {
+                System.out.println("Order with order_id " + orderId + " not found.");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Cập nhật thông tin order
+    public void updateOrder(int orderId, String userId, int packageId, Time pickupTime, String customerName, String customerAddress, String customerPhone) {
+        String sql = "UPDATE orders SET user_id = ?, package_id = ?, pickup_time = ?, customer_name = ?, customer_address = ?, customer_phone = ? WHERE order_id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, userId);
+            ps.setInt(2, packageId);
+            ps.setTime(3, pickupTime);
+            ps.setString(4, customerName);
+            ps.setString(5, customerAddress);
+            ps.setString(6, customerPhone);
+            ps.setInt(7, orderId);
+
+            if (ps.executeUpdate() > 0) {
+                System.out.println("Order updated successfully.");
+            } else {
+                System.out.println("Order with order_id " + orderId + " not found.");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Xóa order
+    public void deleteOrder(int orderId) {
+        String sql = "DELETE FROM orders WHERE order_id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, orderId);
+
+            if (ps.executeUpdate() > 0) {
+                System.out.println("Order deleted successfully.");
+            } else {
+                System.out.println("Order with order_id " + orderId + " not found.");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
--- src/dao/TestMain.java
+++ src/dao/TestMain.java
@@ -12,9 +12,12 @@ public class TestMain {
 
     public static void main(String[] args) {
         PackpageTypeDao packageTypeDao = new PackpageTypeDao();
-//         packageTypeDao.createPackageType(1, "Type 1", "Description 1");
+         packageTypeDao.createPackageType(1, "Type 1", "Description 1");
         packageTypeDao.readPackageType(1);
         packageTypeDao.updatePackageType(1, "Updated Type 1", "Updated Description 1");
-        packageTypeDao.deletePackageType(1);
+//        packageTypeDao.deletePackageType(1);
+
+//        OrderDao order = new OrderDao();
+//        order.insert(1, "", email, pass, phone, seq, ans, address);
     }
 }
